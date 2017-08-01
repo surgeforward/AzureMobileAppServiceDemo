68bc19a1577de2db74eca32952bb56310b728810
Andrew Hoefling <ahoefling@surgeforward.com>
2017-08-01 01:30:36 +0000

    Rebuilt droid project so it is now properly compiling

--- src/AzureMobileAppServiceDemo.Droid/MainActivity.cs
+++ src/AzureMobileAppServiceDemo.Droid/MainActivity.cs
@@ -1,40 +1,33 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
-
 using Microsoft.WindowsAzure.MobileServices;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
-namespace andrew_demo.Droid
+namespace AzureMobileAppServiceDemo.Droid
 {
-	[Activity (Label = "andrew_demo.Droid",
-		Icon = "@drawable/icon",
-		MainLauncher = true,
-		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
-		Theme = "@android:style/Theme.Holo.Light")]
-	public class MainActivity : FormsApplicationActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    [Activity(
+        Label = "AzureMobileAppServiceDemo.Droid", 
+        MainLauncher = true, 
+        Icon = "@drawable/icon",
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        Theme = "@android:style/Theme.Holo.Light")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Initialize Azure Mobile Apps
-			CurrentPlatform.Init();
+            // Initialize Azure Mobile Apps
+            CurrentPlatform.Init();
 
-			// Initialize Xamarin Forms
-			Forms.Init (this, bundle);
+            // Initialize Xamarin Forms
+            Forms.Init(this, bundle);
 
-			// Load the main application
-			LoadApplication (new App ());
-		}
-	}
+            // Load the main application
+            LoadApplication(new App());
+        }
+    }
 }
 
