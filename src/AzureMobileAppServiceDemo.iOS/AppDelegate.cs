d6fbf2c4d10205893ba7a8ffed50e166d7ee76e1
Andrew Hoefling <ahoefling@surgeforward.com>
2017-08-01 01:37:49 +0000

    Fixed issues with iOS project and it is now building

--- src/AzureMobileAppServiceDemo.iOS/AppDelegate.cs
+++ src/AzureMobileAppServiceDemo.iOS/AppDelegate.cs
@@ -1,33 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-
-using Foundation;
-using UIKit;
-
+﻿using Foundation;
 using Microsoft.WindowsAzure.MobileServices;
-
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
-namespace andrew_demo.iOS
+namespace AzureMobileAppServiceDemo.iOS
 {
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : FormsApplicationDelegate
-	{
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			// Initialize Azure Mobile Apps
-			CurrentPlatform.Init();
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            // Initialize Azure Mobile Apps
+            CurrentPlatform.Init();
 
-			// Initialize Xamarin Forms
-			Forms.Init();
+            // Initialize Xamarin Forms
+            Forms.Init();
 
-			LoadApplication(new App ());
+            LoadApplication(new App());
 
-			return base.FinishedLaunching(app, options);
-		}
-	}
+            return base.FinishedLaunching(app, options);
+        }
+    }
 }
+
 
--- src/AzureMobileAppServiceDemo.iOS/Main.cs
+++ src/AzureMobileAppServiceDemo.iOS/Main.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using UIKit;
 
-using Foundation;
-using UIKit;
-
-namespace andrew_demo.iOS
+namespace AzureMobileAppServiceDemo.iOS
 {
-	public class Application
-	{
-		// This is the main entry point of the application.
-		static void Main (string[] args)
-		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main (args, null, "AppDelegate");
-		}
-	}
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
 }
-
